e163a493bd946acdd4986f2100d829666d4ccda1
NURLYKHAN <n.salamatoff@gmail.com>
2020-02-27 16:35:54 +0600

    Update flatArray.js

--- flatArray.js
+++ flatArray.js
@@ -1,7 +1,8 @@
-function flatArr(toFlatArr, flattedArr = []) {
+export default function flatArr(toFlatArr, flattedArr = []) {
   toFlatArr.map((el) => {
     if (el.nodes) {
       flatArr(el.nodes, flattedArr)
+      // delete el.nodes
       flattedArr.push(el)
     } else {
       flattedArr.push(el)
